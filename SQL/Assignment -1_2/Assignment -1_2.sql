95b6ac5f83b17383c54b82dfb8f6114332f6a31c
Phong2002 <https://github.com/Phong2002/ROCKET_15.git>
2021-06-25 09:31:45 +0700

    Update Assignment -1_2.sql

--- SQL/Assignment -1_2/Assignment -1_2.sql
+++ SQL/Assignment -1_2/Assignment -1_2.sql
@@ -7,9 +7,9 @@ CREATE TABLE Trainee(
     Full_Name			VARCHAR(50) NOT NULL,
     Birth_Date			DATE NOT NULL,
     Gender				ENUM('male','female','unknown') NOT NULL,
-    ET_IQ				TINYINT UNSIGNED CHECK(ET_IQ<=20) NOT NULL,
-    ET_Gmath			TINYINT UNSIGNED CHECK(ET_Gmath<=20) NOT NULL,
-    ET_English			TINYINT UNSIGNED CHECK(ET_English<=20) NOT NULL,
+    ET_IQ				TINYINT UNSIGNED NOT NULL,CHECK(ET_IQ<=20) ,
+    ET_Gmath			TINYINT UNSIGNED NOT NULL,CHECK(ET_Gmath<=20) ,
+    ET_English			TINYINT UNSIGNED NOT NULL,CHECK(ET_English<=20) ,
     Trainning_Class		CHAR(20) NOT NULL,
     Evaluation_Notes	VARCHAR(255) NOT NULL,
     PRIMARY KEY (TraineeID)
